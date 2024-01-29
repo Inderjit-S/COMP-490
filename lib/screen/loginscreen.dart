f1d65db97745152a2212a5f51350b922f684647d
Keith <kitchua12@gmail.com>
2024-01-28 19:43:53 -0800

    first commit

---
+++ lib/screen/loginscreen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import '../reusable_widget/reusable_widget.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key:key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController _passwordTextController = TextEditingController();
+  TextEditingController _emailTextController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: const BoxDecoration(
+          gradient: LinearGradient(colors: [
+            Color(0xFFA4A0A0),
+            Color(0xFF8E9393),
+            Color(0xFFA4A0A0)],
+          begin: Alignment.topCenter, end: Alignment.bottomCenter),),
+      child:  Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+            logoWidget("background_image/aerogotchi.png"),
+              SizedBox(
+                height:10,
+              ),
+              reusableTextField("Enter UserName", Icons.person_outline, false, _emailTextController),
+              SizedBox(
+                height:10,
+              ),
+          reusableTextField("Enter Password", Icons.lock_outline, true, _passwordTextController),
+        ],
+              ),
+              ),
+          ),
+        );
+  }
+}
