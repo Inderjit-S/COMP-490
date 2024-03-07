216047c97c60e62017bdc2ad38bd7152cbbcd389
Peter Jay Hernandez <peter.j.hernandez01@gmail.com>
2024-03-07 14:13:03 -0800

    FaceTracking update

--- aero_flight/faceTracking.py
+++ aero_flight/faceTracking.py
@@ -18,24 +18,24 @@ fbRange = [6200, 6800]
 pid = [0.4, 0.4, 0] # play around with these values for smoothness
 pError = 0
 
-def adjustTello(offset_x,offset_y,offset_z):
-    if not -90 <= offset_x <= 90 and offset_x is not 0:
-        if offset_x < 0:
-            tello.rotate_counter_clockwise(10)
-        elif offset_x > 0:
-            tello.rotate_clockwise(10)
+# def adjustTello(offset_x,offset_y,offset_z):
+#     if not -90 <= offset_x <= 90 and offset_x is not 0:
+#         if offset_x < 0:
+#             tello.rotate_counter_clockwise(10)
+#         elif offset_x > 0:
+#             tello.rotate_clockwise(10)
 
-    if not -70 <= offset_y <= 70 and offset_y is not -30:
-        if offset_y < 0:
-            tello.move_up(20)
-        elif offset_y > 0:
-            tello.move_down(20)
+#     if not -70 <= offset_y <= 70 and offset_y is not -30:
+#         if offset_y < 0:
+#             tello.move_up(20)
+#         elif offset_y > 0:
+#             tello.move_down(20)
 
-    if not 15000 <= offset_z <= 30000 and offset_z is not 0:
-        if offset_z < 15000:
-            tello.move_forward(20)
-        elif offset_z > 30000:
-            tello.move_back(20)
+#     if not 15000 <= offset_z <= 30000 and offset_z is not 0:
+#         if offset_z < 15000:
+#             tello.move_forward(20)
+#         elif offset_z > 30000:
+#                 tello.move_back(20)
 
 def findFace(img):
     #faceCascadeName = cv2.data.haarcascades + 'haarcascadefrontalface_alt.xml'
@@ -83,7 +83,7 @@ def tracking(info, w, pid, pError):
 
     # PID for up/down
     error_ud = y - h//2
-    speed_ud = pid[2] * error_ud + pid[3] * (error_ud-pError)
+    speed_ud = pid[0] * error_ud + pid[1] * (error_ud-pError)
     speed_ud = int(np.clip(speed_ud, -100, 100))
 
     area = info[1]
@@ -102,7 +102,7 @@ def tracking(info, w, pid, pError):
     #print(speed, fb)
 
     #tello.send_rc_control(0, fb, 0, speed)
-    tello.send_rc_control(0, fb, 0, speed, 0, speed_ud)
+    tello.send_rc_control(0, fb, speed_ud, speed)
     return error
     #return error, error_ud
 
