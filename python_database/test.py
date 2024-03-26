51df3a98582904acf78804138a9313f7752d27e1
Inderjit Singh <chansingh02@yahoo.com>
2024-03-25 19:35:21 -0700

    created methods for updating energy level to drones battery in database

--- python_database/test.py
+++ python_database/test.py
@@ -1,5 +1,11 @@
 import firebase_admin
+from djitellopy import Tello
 from firebase_admin import db, credentials
+from time import sleep
+
+# connect drone
+tello = Tello()
+tello.connect()
 
 # Initialize Firebase app with provided credentials
 cred = credentials.Certificate("python_database\\cred.json")
@@ -10,9 +16,22 @@ ref = db.reference("/")
 data = ref.get()
 print(data)  # Print the retrieved data
 
-# Update Energy Level
- 
-db.reference("/").update({"energy_level": 5})
-data = ref.get()
+# Update Energy Level Method 1
+while True:
+    db.reference("/").update({tello.get_battery})
+    data = ref.get()
+    print(data)
+    sleep(5) # update firebase database every 5 seconds. Increase later
 
-print(data)  # Print the retrieved data
+# # Update Energy Level Method 2
+# while True:
+#     battery = tello.get_battery
+#     ref.update({'battery': battery})
+#     sleep(5) # update firebase database every 5 seconds. Increase later
+
+
+# Manual update for energy level
+# db.reference("/").update({"energy_level": 5})
+# data = ref.get()
+
+# print(data)  # Print the retrieved data
