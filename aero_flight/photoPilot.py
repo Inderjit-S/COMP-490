347d2dab1fd4bf7b9cb99777c563b45a60aca4ba
Peter Jay Hernandez <peter.j.hernandez01@gmail.com>
2024-05-07 14:01:27 -0700

    Tested PhotoPilot and notes on next adjustments

--- aero_flight/photoPilot.py
+++ aero_flight/photoPilot.py
@@ -6,18 +6,24 @@ import time
 telloInstance = tello.Tello()
 telloInstance.connect()
 bat = print(telloInstance.get_battery())
+telloInstance.streamon()
 
 w = 360
 h = 240
 
 while True:
     img = telloInstance.get_frame_read().frame # read frame
-    resize = cv2.resize(img, (w,h))
+    img = cv2.resize(img, (w,h))
     
     cv2.imshow("Output", img) # create output window
     if cv2.waitKey(1) & 0xFF == ord('s'): # press S for picture
-        cv2.imwrite("test.jpg", resize)
+        test = cv2.imwrite("test.jpg", img)
         print("Picture Taken")
+        test = cv2.resize(test, (w,h))
+        cv2.imshow("Output", test)
+        time.sleep(5)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         telloInstance.land()
         break
+
+# draw something on the output picture and change variable names.
