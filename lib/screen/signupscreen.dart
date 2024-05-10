d44f087ecc0c440cf46a538c7b3d50d9202e16dc
Keith <kitchua12@gmail.com>
2024-05-09 23:46:25 -0700

    fixed routing issue

--- lib/screen/signupscreen.dart
+++ lib/screen/signupscreen.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import 'loginscreen.dart';
 import 'logoutscreen.dart';
 
 class SignUpScreen extends StatefulWidget {
@@ -66,7 +67,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
                         Navigator.push(
                           context,
                           MaterialPageRoute(
-                            builder: (context) => LogoutScreen(petName: '',),
+                            builder: (context) => LoginScreen(),
                           ),
                         );
                       }).catchError((error) {
