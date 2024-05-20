c4a1b2e97694737b51e0ee9f9ac8c6f0c7a8b6d1
roiidd1 <zamir.barbosa.112@my.csun.edu>
2024-05-20 14:38:41 -0700

    updated aerofacesscreen.dart

--- lib/screen/aerofacesscreen.dart
+++ lib/screen/aerofacesscreen.dart
@@ -11,49 +11,75 @@ class AeroFacesScreen extends StatelessWidget {
         petName: '',
       ),
       extendBodyBehindAppBar: true,
-      body: Stack(
-        children: [
-          Container(
-            width: double.infinity,
-            height: double.infinity,
-            decoration: BackgroundGradient.blueGradient,
-          ),
-          Positioned.fill(
-            top: MediaQuery.of(context).padding.top + 80,
-            child: Column(
-              children: [
-                const SizedBox(height: 30),
-                Expanded(
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        GridView.count(
-                          crossAxisCount: 2,
-                          children: <Widget>[
-                            buildImageWithText('assets/images/aerogotchi.png', 'Not Hungry\nNot Excited'),
-                            buildImageWithText('assets/images/aeroexcited.png', 'Not Hungry\nExcited'),
-                            buildImageWithText('assets/images/aerounhappy.png', 'Hungry'),
-                            buildImageWithText('assets/images/aerosleepy.png', 'Low Battery'),
-                          ],
-                        ),
-                        const SizedBox(height: 20),
-                        TextField(
-                          decoration: InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'Aerogotchi will display one of four faces based on its current status. You can check the status levels in the Pet Status Menu designated by the green icon with the ECG symbol.',
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Stack(
+          children: [
+            Container(
+              width: double.infinity,
+              height: double.infinity,
+              decoration: BackgroundGradient.blueGradient,
+            ),
+            Positioned.fill(
+              top: MediaQuery.of(context).padding.top,
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          GridView.count(
+                            crossAxisCount: 2,
+                            shrinkWrap: true,
+                            children: <Widget>[
+                              buildImageWithText('background_image/aerogotchi.png', 'Not Hungry\nNot Excited'),
+                              buildImageWithText('background_image/aeroexcited.png', 'Not Hungry\nExcited'),
+                              buildImageWithText('background_image/aerounhappy.png', 'Hungry'),
+                              buildImageWithText('background_image/aerosleepy.png', 'Low Battery'),
+                            ],
                           ),
-                        ),
-                        const SizedBox(height: 20),
-                      ],
+                          const SizedBox(height: 20),
+                          Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.white.withOpacity(0.7)),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            padding: const EdgeInsets.all(10),
+                            child: Column(
+                              children: [
+                                Text(
+                                  'Aerogotchi will display one of four faces based on its current status.',
+                                  style: TextStyle(
+                                    color: Colors.white.withOpacity(0.7),
+                                    fontSize: 20,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                                const SizedBox(height: 20),
+                                Text(
+                                  'You can check the status levels in the Pet Status Menu designated by the green icon with the ECG symbol.',
+                                  style: TextStyle(
+                                    color: Colors.white.withOpacity(0.7),
+                                    fontSize: 20,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
@@ -63,7 +89,14 @@ class AeroFacesScreen extends StatelessWidget {
       child: Column(
         children: <Widget>[
           Image.asset(imagePath),
-          Text(text),
+          Text(
+            text,
+            style: TextStyle(
+               color: Colors.white.withOpacity(0.85),
+
+              fontWeight: FontWeight.bold,
+            ),
+          ),
         ],
       ),
     );
